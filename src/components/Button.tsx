6168ed070d874cfe43e6f7a695889656b82ed658
anikas <22bec020@nith.ac.in>
2025-05-23 10:17:56 +0530

    Ux fixed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,14 +8,13 @@ interface ButtonProps {
 
 // Global style mapping for variants
 const variantClasses: Record<"primary" | "secondary", string> = {
-  primary: "bg-purple-600 text-white px-4 py-2 rounded",
-  secondary: "bg-purple-200 text-purple-600 px-4 py-2 rounded",
+  primary: "bg-purple-600 text-white ",
+  secondary: "bg-purple-200 text-purple-600 ",
 };
-
+const defaultstyles = "px-4 py-2 rounded md rounded-md font-light";
 export function Button({ variant, text, startIcon }: ButtonProps) {
   return (
-    <button className={variantClasses[variant]}>
-      {startIcon && <span className="mr-2">{startIcon}</span>}
+    <button className={variantClasses[variant] + "" + defaultstyles}>
       {text}
     </button>
   );
